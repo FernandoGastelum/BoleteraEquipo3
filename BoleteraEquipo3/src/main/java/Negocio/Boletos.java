c97ad923a29b2fdcc8f6fb22709747f815fb6dbc
FernandoGastelum <ilian.gastelum228761@potros.itson.edu.mx>
2024-09-24 00:58:32 -0700

    Clases Negocio
    Se agregaron algunas clases de negocio con su respectivo codigo provisional

--- BoleteraEquipo3/src/main/java/Negocio/Boletos.java
+++ BoleteraEquipo3/src/main/java/Negocio/Boletos.java
@@ -9,5 +9,121 @@ package Negocio;
  * @author Equipo3
  */
 public class Boletos {
-    
+    private int boletoId;
+    private String numeroSerie;
+    private String fila;
+    private String asiento;
+    private int numeroControl;
+    private double precioOriginal;
+    private int eventoId;
+    private int usuarioId;
+    private boolean reventa;
+    private boolean venta;
+
+    // Constructor vacío
+    public Boletos() {}
+
+    // Constructor con todos los parámetros
+    public Boletos(int boletoId, String numeroSerie, String fila, String asiento, int numeroControl, double precioOriginal, int eventoId, int usuarioId, boolean reventa, boolean venta) {
+        this.boletoId = boletoId;
+        this.numeroSerie = numeroSerie;
+        this.fila = fila;
+        this.asiento = asiento;
+        this.numeroControl = numeroControl;
+        this.precioOriginal = precioOriginal;
+        this.eventoId = eventoId;
+        this.usuarioId = usuarioId;
+        this.reventa = reventa;
+        this.venta = venta;
+    }
+
+    // Getters y Setters
+    public int getBoletoId() {
+        return boletoId;
+    }
+
+    public void setBoletoId(int boletoId) {
+        this.boletoId = boletoId;
+    }
+
+    public String getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setNumeroSerie(String numeroSerie) {
+        this.numeroSerie = numeroSerie;
+    }
+
+    public String getFila() {
+        return fila;
+    }
+
+    public void setFila(String fila) {
+        this.fila = fila;
+    }
+
+    public String getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(String asiento) {
+        this.asiento = asiento;
+    }
+
+    public int getNumeroControl() {
+        return numeroControl;
+    }
+
+    public void setNumeroControl(int numeroControl) {
+        this.numeroControl = numeroControl;
+    }
+
+    public double getPrecioOriginal() {
+        return precioOriginal;
+    }
+
+    public void setPrecioOriginal(double precioOriginal) {
+        this.precioOriginal = precioOriginal;
+    }
+
+    public int getEventoId() {
+        return eventoId;
+    }
+
+    public void setEventoId(int eventoId) {
+        this.eventoId = eventoId;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public boolean isReventa() {
+        return reventa;
+    }
+
+    public void setReventa(boolean reventa) {
+        this.reventa = reventa;
+    }
+
+    public boolean isVenta() {
+        return venta;
+    }
+
+    public void setVenta(boolean venta) {
+        this.venta = venta;
+    }
+
+    // Método toString para imprimir los detalles del boleto
+    @Override
+    public String toString() {
+        return "Boleto [boletoId=" + boletoId + ", numeroSerie=" + numeroSerie + ", fila=" + fila + ", asiento=" + asiento
+                + ", numeroControl=" + numeroControl + ", precioOriginal=" + precioOriginal + ", eventoId=" + eventoId
+                + ", usuarioId=" + usuarioId + ", reventa=" + reventa + ", venta=" + venta + "]";
+    }
 }
+
---
+++ BoleteraEquipo3/src/main/java/Negocio/Usuarios.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Negocio;
+import java.util.Date;
+/**
+ *
+ * @author gaspa
+ */
+public class Usuarios {
+    private int usuarioId;
+    private String correo;
+    private String contrasena; // En español sería "contraseña", pero por convención en Java usamos nombres sin caracteres especiales
+    private String nombreCompleto;
+    private String domicilio;
+    private Date fechaNacimiento;
+    private int edad;
+    private double saldo;
+
+    // Constructor vacío
+    public Usuarios() {}
+
+    // Constructor con todos los parámetros
+    public Usuarios(int usuarioId, String correo, String contrasena, String nombreCompleto, String domicilio, Date fechaNacimiento, int edad, double saldo) {
+        this.usuarioId = usuarioId;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.nombreCompleto = nombreCompleto;
+        this.domicilio = domicilio;
+        this.fechaNacimiento = fechaNacimiento;
+        this.edad = edad;
+        this.saldo = saldo;
+    }
+
+    // Getters y Setters
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    // Método toString para imprimir los detalles del usuario
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "usuarioId=" + usuarioId +
+                ", correo='" + correo + '\'' +
+                ", contraseña='" + contrasena + '\'' +
+                ", nombreCompleto='" + nombreCompleto + '\'' +
+                ", domicilio='" + domicilio + '\'' +
+                ", fechaNacimiento='" + fechaNacimiento + '\'' +
+                ", edad=" + edad +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
+
